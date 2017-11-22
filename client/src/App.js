4bfe5f0aa20817c58643dcb9c77bd09c91d2100b
Ed Orsini <edorsini@mit.edu>
2017-11-22 17:06:45 -0500

    working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Pokedex from "./pages/Pokedex";
 // import Home from "./pages/Home";
 // import Game from "./pages/Game";
@@ -7,18 +8,24 @@ import Pokedex from "./pages/Pokedex";
 // import Vault from "./pages/Vault";
 
 const App = () =>
-	<Router>
+  <Router>
     <div>
-    <Switch>
-      <Route exact path="/" component={Pokedex} />
-      <Route exact path="/pokedex" component={Pokedex} />
-      <Route exact path="/home" component={Pokedex} />
-      <Route exact path="/profile" component={Pokedex} />      
-      <Route exact path="/pokedex" component={Pokedex} />
-      <Route exact path="/game" component={Pokedex} />  
-      <Route exact path="/vault" component={Pokedex} /> 
-      <Route component={Pokedex} />   
-    </Switch>
+
+      
+      {
+        // This is where React Router will match the path.  If a path is
+        // matched, the associated component will be rendered.
+        // For example, if "/books" is matched, the Books component will
+        // be rendered.
+      }
+      <Switch>
+        <Route exact path="/" component={Pokedex} />
+        <Route exact path="/home" component={Pokedex} />
+        <Route exact path="/pokedex" component={Pokedex} />
+        {
+          // If no match, render the NoMatch component
+        }
+      </Switch>
 
     </div>
   </Router>;
