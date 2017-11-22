ace73d7fb5133c67753c62c89b184660f5457399
Ed Orsini <edorsini@mit.edu>
2017-11-22 16:50:09 -0500

    working

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const pokedexRoutes = require("./pokedex");
-const authRoutes = require("./auth-routes");
-const profileRoutes = require("./profile-routes");
+// const authRoutes = require("./auth-routes");
+// const profileRoutes = require("./profile-routes");
 
 router.use("/pokedex", pokedexRoutes);
 router.use("/auth", authRoutes);
