f71252e9390cbcdec4d8da5ff4d4c0f45f2423ef
Ed Orsini <edorsini@mit.edu>
2017-11-22 16:05:32 -0500

    working

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Pokedex from "./pages/Pokedex";
-import Home from "./pages/Home";
-import Game from "./pages/Game";
-import Profile from "./pages/Profile";
-import Vault from "./pages/Vault";
+// import Home from "./pages/Home";
+// import Game from "./pages/Game";
+// import Profile from "./pages/Profile";
+// import Vault from "./pages/Vault";
 
 const App = () =>
 	<Router>
     <div>
       <Route exact path="/" component={Pokedex} />
       <Route exact path="/pokedex" component={Pokedex} />
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/profile" component={Profile} />      
+      <Route exact path="/home" component={Pokedex} />
+      <Route exact path="/profile" component={Pokedex} />      
       <Route exact path="/pokedex" component={Pokedex} />
-      <Route exact path="/game" component={Game} />  
-      <Route exact path="/vault" component={Vault} />      
+      <Route exact path="/game" component={Pokedex} />  
+      <Route exact path="/vault" component={Pokedex} />      
           
     </div>
   </Router>;
