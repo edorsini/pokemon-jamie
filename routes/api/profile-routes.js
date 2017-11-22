51415d879afd76835163106d9e805e95275d89a3
Ed Orsini <edorsini@mit.edu>
2017-11-22 17:41:30 -0500

    working

--- routes/api/profile-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const profileController = require('../../controllers//profileController');
-
-router.route('/user')
-  .get(profileController.getUser)
-  .post(profileController.updateCookie)
-  .get(profileController.getHighScores)
-
-module.exports = router;
